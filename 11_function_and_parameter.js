e4c1a30d9f937a2944bdf7821062617e583d38bd
veenayaksirohi <113943894+veenayaksirohi@users.noreply.github.com>
2025-07-05 16:35:20 +0530

    Add variable scope demonstration with let and const
    This update includes a new section illustrating block scope behavior and variable shadowing using let and const. It features examples of global and block-scoped variables, along with comments on potential ReferenceErrors for undeclared variables.

--- 11_function_and_parameter.js
+++ 11_function_and_parameter.js
@@ -121,3 +121,20 @@ function handleobject (user) {
 
 const userInfo = handleobject(user)
 console.log('handleobject result:', userInfo)
+
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    console.log("INNER: ", a);
+    
+}
+
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+
